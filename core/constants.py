88425c1d11948d04af59b21fe229689ae44374fe
prarit0097 <1995praritsidana@gmail.com>
2026-03-10 14:23:59 +0530

    Limit Meta OAuth scopes to page management only

--- core/constants.py
+++ core/constants.py
@@ -22,8 +22,4 @@ META_SCOPES = [
     "pages_show_list",
     "pages_read_engagement",
     "pages_manage_posts",
-    "pages_read_user_content",
-    "instagram_basic",
-    "instagram_content_publish",
-    "instagram_manage_insights",
 ]
